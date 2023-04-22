ece514b825ff4cded65d2ac1fee6568c61cac878
Kimmy7070 <82769101+Kimmy7070@users.noreply.github.com>
2023-04-23 01:20:50 +0530

    23-04-2023 (header and contact us update)

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -6,51 +6,60 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     {{-- Bootstrap cdn --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
 
     {{-- custom css --}}
     <link rel="stylesheet" href="{{asset('css/contactus.css')}}">
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="{{asset('css/index.css')}}" />
+
+    <!-- font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- fontawsome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <!-- boxicons -->
+
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"
+    />
 
     <title>Contact Us</title>
 </head>
 <body>
     {{-- navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+    <header>
+      <a href="" class="logo"><img src="./img/logo.png" alt="" /></a>
+      <ul class="search-bar">
+        <input type="search" class="search" placeholder="Search" />
+        <a href="#"><i class="bx bx-search"></i></a>
+      </ul>
+
+      <div class="nav-icon">
+        <div class="icons-container">
+          <a href=""><i class="bx bx-heart"></i></a>
+          <a href="#"><i class="bx bx-cart"></i></a>
+          <a href=""><i class="bx bx-bell"></i></a>
+          <a href="#"><i class="bx bx-user-circle"></i></a>
         </div>
-      </nav>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+      </div>
+    </header>
 
     {{-- section of form and map --}}
     <section>
@@ -88,5 +97,6 @@
     </div>
 
 </section>
+<script src="{{asset('js/index.js')}}"></script>
 </body>
 </html>
