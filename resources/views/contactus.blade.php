142a8a62ee492c3d015774136724e1555c30281b
Kimmy7070 <82769101+Kimmy7070@users.noreply.github.com>
2023-04-23 02:19:08 +0530

    23-04-2023 (small changes)

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -65,29 +65,24 @@
     <section>
         <div class="full-container">
 
+            {{-- form integration --}}
             <div class="left-container">
-                <h2>Contact Us</h2>
-                {{-- <h6>  Full name </h6> --}}
+                <h1>Contact Us</h1>
                 <div class="input-group mb-3">
-              {{-- <span class="input-group-text" id="basic-addon1">Name </span> --}}
               <input type="text" class="form-control" placeholder="First name" aria-label="Username" aria-describedby="basic-addon1">
             </div>
 
-             {{-- <h6>  Your email</h6> --}}
             <div class="input-group mb-3">
               <input type="text" class="form-control" placeholder="Your email" aria-label="Recipient's username" aria-describedby="basic-addon2">
-              {{-- <span class="input-group-text" id="basic-addon2">@example.com</span> --}}
             </div>
 
-            {{-- <h6>  Your query</h6> --}}
             <div class="input-group mb-3">
-              {{-- <span class="input-group-text">Your query</span> --}}
               <textarea class="form-control" placeholder="Your query" aria-label="With textarea"></textarea>
             </div>
 
-            {{-- <button type="button" class="btn btn-light">Light</button> --}}
             <input class="btn btn-success" type="submit" value="Submit">
             </div>
+
             {{-- map integration --}}
             <div class="right-container">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.5902933229836!2d88.37954111496536!3d22.891588485019646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89167529a8f17%3A0xe94a8f21ca53259a!2sTechno%20India%20Hooghly!5e0!3m2!1sen!2sin!4v1682076381784!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
