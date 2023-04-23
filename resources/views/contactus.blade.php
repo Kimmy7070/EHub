8c415977efa255ffd35e084781301dd03ed48214
Kimmy7070 <82769101+Kimmy7070@users.noreply.github.com>
2023-04-23 12:43:55 +0530

    contact ->comment change

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -8,10 +8,10 @@
     {{-- Bootstrap cdn --}}
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
 
-    {{-- navbar css --}}
     {{-- custom css --}}
     <link rel="stylesheet" href="{{asset('css/contactus.css')}}">
 
+    {{-- navbar css --}}
     <!-- custom css -->
     <link rel="stylesheet" href="{{asset('css/index.css')}}" />
 
