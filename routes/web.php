a97e3387de42d581ebbc1c3209d8dd0d5f41a929
Kimmy7070 <82769101+Kimmy7070@users.noreply.github.com>
2023-04-28 14:35:44 +0530

    admin layout U and error M and web.php U 28/4/23

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('contactus', function () {
     return view('contactus');
 });
 
-Route::get('contactus_backend_api', [App\Http\Controllers\contactus_controller::class, 'contactus_backend_ganja']);
+Route::get('contactus_backend_api', [contactus_controller::class, 'contactus_backend_ganja']);
 
 Auth::routes();
 
@@ -34,6 +34,37 @@ Route::get('/testing', function(){
     return view('testing');
 });
 
+// Admin view
+
 Route::get('/admin', function(){
     return view('/admin/admin');
+});
+
+Route::get('/admin/contact', function(){
+    return view('/admin/pages-contact');
+});
+
+Route::get('/admin/forms', function(){
+    return view('/admin/forms-elements');
+});
+
+Route::get('/admin/error', function(){
+    return view('/admin/pages-error-404');
+});
+
+Route::get('/admin/faq', function(){
+    return view('/admin/pages-faq');
+});
+
+Route::get('/admin/tables', function(){
+    return view('admin.tables-data');
+});
+
+Route::get('/admin/user', function(){
+    return view('/admin/users-profile');
+});
+
+
+Route::get('/admin/layout', function(){
+    return view ('/admin/layout');
 });
